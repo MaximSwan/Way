796f8b337ccdbe2256f7faa549dc7692e4857d3b
Maxim <max777.lebed@yandex.ru>
2018-08-22 15:23:22 +0300

    backEnd

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,13 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var db = require('./db/dbFolder.js')
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,22 +16,47 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/getAllFolders', async (req, res) => {
+  let folders = await db.Folder.find({});
+  res.send(folders);
+})
+
+app.post('/addNewFolder/:name/:parentName', (req, res) => {
+  let folder = new db.Folder();
+  folder.name = req.params.name;
+  folder.parentName = req.params.parentName;
+  folder.save()
+    .then(result => {
+      res.send(folder)
+    })
+    .catch(err => {
+      console.error(err);
+    });
+})
+
+app.post('/addNewFile/:fileName/:idParent', async (req, res) => {
+  let file = new db.File();
+  file.name = req.params.fileName;
+  file.idParent = req.params.idParent;
+  file.save().then(result => {
+    res.send(file)
+  } )  
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.delete('/deleteFolder/:id', (req, res) => {
+  db.Folder.deleteOne({_id:req.params.id})
+  .then(result => {
+    res.send('Deleted');
+  } )
+} )
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.delete('/deleteFile/:id', (req, res) => {
+   db.File.deleteOne({_id:req.params.id})
+   .then(result => {
+     res.send('deleted')
+   } )
+})
+
 
 module.exports = app;
---
+++ backend/db/dbFolder.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Folders')
+  .then(() => {
+    console.log('Successful connection');
+  })
+  .catch(err => {
+    console.error('Can t connect to db');
+  })
+
+const Folder = mongoose.model('Folder', {
+  name: String,
+  parentName:String
+})
+
+const File = mongoose.model('File', {
+  name:String,
+  idParent:String
+})
+
+module.exports = {
+  Folder: Folder,
+  File: File,
+}
